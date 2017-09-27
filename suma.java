40eeee10e19bbb1d667024de3948f8ae5f4c070f
Arnau Risquez <arisquezg@gmail.com>
2017-09-27 16:31:32 +0200

    pujem el fitxer java
    pujem el fitxer suma.java

---
+++ suma.java
@@ -0,0 +1,8 @@
+int numero1= 0;
+int numero2= 0;
+
+System.out.println("introduce el primer número:");
+numero1 = reader.nextint();
+
+System.out.println("introduce el segundo número:");
+numero2 = reader.nextint();
